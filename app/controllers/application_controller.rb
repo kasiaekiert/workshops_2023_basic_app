fcf2537c0d0c3316033c1603808c6cc3c97420f3
Edyta Barańska <edyta.baranska@infakt.pl>
2023-05-08 12:59:58 +0200

    helper methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,17 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
-  before_action :load_weather_data
-  before_action :load_presenter
 
   def flash_notice
     I18n.t(:notice, scope: [:controllers, controller_name, action_name])
-  end
-
-  def load_weather_data
-    @data = WeatherApiConnector.new.weather_data
-  end
-
-  def load_presenter
-    @presenter ||= WeatherPresenter.new(@data)
   end
 end
