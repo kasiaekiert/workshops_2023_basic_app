b7523ba442530cacc9ad25bd4bb7d98f35f8bd04
multikasia <kasia.ekiert@gmail.com>
2023-10-25 11:16:34 +0200

    gem bunny

--- Gemfile
+++ Gemfile
@@ -2,7 +2,7 @@ source 'https://rubygems.org'
 git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 ruby '3.2.1'
-
+gem 'bunny'
 gem 'bootsnap', require: false
 gem 'bootstrap'
 gem 'devise'
--- app/controllers/book_loans_controller.rb
+++ app/controllers/book_loans_controller.rb
@@ -5,6 +5,7 @@ class BookLoansController < ApplicationController
   def create
     respond_to do |format|
       if @book_loan.save
+        publish_log(@book_loan)
         format.html { redirect_to book_url(book), notice: flash_notice }
         format.json { render :show, status: :created, location: @book_loan }
       else
@@ -37,5 +38,9 @@ class BookLoansController < ApplicationController
 
   def book_loan_params
     params.require(:book_id)
+  end
+
+  def publish_log(book_loan)
+    Publishers::LoanBookPublisher.new(book_loan.attributes).publish
   end
 end
