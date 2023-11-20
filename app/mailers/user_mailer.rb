ab3835ca816d08f5bc607defbd57d6d409175354
multikasia <kasia.ekiert@gmail.com>
2023-11-20 10:03:58 +0100

    update

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,17 +1,17 @@
 class UserMailer < ApplicationMailer
-  def loan_created_email(book_load)
-    @title = book_load.book.title
-    @date = book_load.created_at
-    @due_date = book_load.due_date
-    @email = book_load.user.email
+  def loan_created_email(book_loan)
+    @title = book_loan.book.title
+    @date = book_loan.created_at
+    @due_date = book_loan.due_date
+    @email = book_loan.user.email
     @email_subject = "New action in your account"
     mail(to: @email, subject: @email_subject)
   end
 
   def due_date_notification_email
-    @title = book_load.book.title
-    @due_date = book_load.due_date
-    @email = book_load.user.email
+    @title = book_loan.book.title
+    @due_date = book_loan.due_date
+    @email = book_loan.user.email
     @email_subject = "ALERT"
     mail(to: @email, subject: @email_subject)
   end
